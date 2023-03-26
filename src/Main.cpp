1f8d8b1564067b9ebf720ce8ae7bcc8b550e79dd
Jovan <jormundur00@gmail.com>
2023-03-26 20:55:26 +0200

    feature: Implemented primitive player movement, boundaries and animation
    Implemented the most basic player movement for two players, using WASD and Arrow Keys. The current movement system doesn't support gravity or any real physics system.
    Added boundaries for said players, so that leaving the screen is currently impossible.
    Added basic test sprites and a primitive way to animate movement and staying still.
    The entire game logic is currently in the Main.cpp file, which should be refactored once an agreement on project structure is made.

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,170 +1,98 @@
 #pragma once
 
 #include "Prelude.h"
-#include "Random.h"
 
 using namespace core;
 
-struct PadCollisionSignal {
-	ecs::Entity ball;
-	ecs::Entity pad;
+// TODO: Create Ground struct and enable collision
+struct GroundCollisionSignal {
+	ecs::Entity player;
 };
 
 struct Player {};
 
-struct Ball 
-{
-	geometry::Vec2 speed;
-};
-
-struct KeyBindings 
-{
+struct KeyBindings {
 	KeyCode up;
 	KeyCode down;
-};
-
-struct PhysicsSystem
-	: public ecs::System
-	, public AccessGroupStorage<Player, Position>
-	, public AccessGroupStorage<Ball, Position>
-	, public SignalEmitter<PadCollisionSignal>
-{
-	using QueryPlayers = AccessGroupStorage<Player, Position>;
-	using QueryBalls = AccessGroupStorage<Ball, Position>;
-
-	Bool intersects(const geometry::Vec2& pad_center, const geometry::Vec2& ball_center, float ball_radius)
-	{
-		static constexpr const int pad_width = 11;
-		static constexpr const int pad_height = 32;
-
-		const geometry::Vec2 circle_distance{
-			abs(ball_center.x - pad_center.x),
-			abs(ball_center.y - pad_center.y)
-		};
-
-		if (circle_distance.x > (pad_width + ball_radius)) { return false; }
-		if (circle_distance.y > (pad_height + ball_radius)) { return false; }
-
-		if (circle_distance.x <= pad_width) { return true; }
-		if (circle_distance.y <= pad_height) { return true; }
-
-		const auto dx = circle_distance.x - pad_width;
-		const auto dy = circle_distance.y - pad_height;
-		const auto corner_distance = dx * dx + dy * dy;
-
-		return corner_distance <= ball_radius * ball_radius;
-	}
-
-	void on_tick() override
-	{
-		for (auto&& [ball_entity, ball, pos] : QueryBalls::access_storage().each())
-		{
-			for (auto&& [player_entity, pad] : QueryPlayers::access_storage().each())
-			{
-				if (intersects(pad.xy, pos.xy, 16))
-				{
-					emit(PadCollisionSignal{ ball_entity, player_entity });
-				}
-			}
-		}
-	}
+	KeyCode left;
+	KeyCode right;
 };
 
 #define SCREEN_WIDTH 800
 #define SCREEN_HEIGHT 600
-#define BALL_RADIUS 16
-#define SPEED_MOD 200.0f
+#define SPEED_MOD 300.0f
 
-struct BallMovementSystem
+struct PlayerControlsSystem 
 	: public ecs::System
-	, public MutAccessGroupStorage<Ball, Position>
-	, public MutAccessComponentById<Ball>
-	, public SignalProcessor<PadCollisionSignal>
-{
-	virtual void process_signal(PadCollisionSignal& signal)
-	{
-		auto& ball = MutAccessComponentById<Ball>::get(signal.ball);
-		ball.speed.x *= -1;
-		ball.speed.y = get_random(-0.8f, 0.8f);
-	}
-
-	void on_tick() override
-	{
-		for (auto&& [entity, ball, pos] : access_storage().each())
-		{
-			if (pos.xy.y >= SCREEN_HEIGHT - BALL_RADIUS || pos.xy.y <= BALL_RADIUS)
-			{
-				ball.speed.y *= -1;
-			}
-
-			pos.xy += ball.speed * SPEED_MOD * Time::delta_time();
-		}
-	}
-};
-
-struct PlayerControlsSystem
-	: public ecs::System
-	, public MutAccessGroupStorage<Player, KeyBindings, Position>
+	, public MutAccessGroupStorage<Player, KeyBindings, Position, SpriteAnimation>
 {
 	void on_tick() override
 	{
 		const auto& keys = KeyState::get();
 
-		if (keys.is_pressed(KEY_ESCAPE))
-		{
-			Engine::get_instance().quit();
-		}
-
-		for (auto&& [entity, bindings, pos] : access_storage().each())
+		for (auto&& [entity, bindings, pos, sprite] : access_storage().each())
 		{
 			if (keys.is_down(bindings.up))
 			{
 				pos.xy.y -= SPEED_MOD * Time::delta_time();
+				if (pos.xy.y < 16) {
+					pos.xy.y = 16;
+				}
 			}
 			else if (keys.is_down(bindings.down))
 			{
 				pos.xy.y += SPEED_MOD * Time::delta_time();
+				if (pos.xy.y > SCREEN_HEIGHT - 16) {
+					pos.xy.y = SCREEN_HEIGHT - 16;
+				}
+			}
+			else if (keys.is_down(bindings.left))
+			{
+				sprite.change_to("test/WizardRun");
+				pos.xy.x -= SPEED_MOD * Time::delta_time();
+				if (pos.xy.x < 11) {
+					pos.xy.x = 11;
+				}
+			}
+			else if (keys.is_down(bindings.right))
+			{
+				sprite.change_to("test/WizardRun");
+				pos.xy.x += SPEED_MOD * Time::delta_time();
+				if (pos.xy.x > SCREEN_WIDTH - 11) {
+					pos.xy.x = SCREEN_WIDTH - 11;
+				}
+			}
+			else {
+				sprite.change_to("test/WizardIdle");
 			}
 		}
 	}
 };
 
-struct Pong : public Game
-{
-	Pong()
+struct SWMG : public Game {
+	SWMG()
 	{
 		auto& engine = Engine::get_instance();
 		engine.use<PlayerControlsSystem>();
-		engine.use<BallMovementSystem>();
-		engine.use<PhysicsSystem>();
 	}
 
-	void on_start() override
-	{
-		auto pad_left = spawn()
+	void on_start() override{
+		auto player_one = spawn()
 			.with<Player>()
 			.with<Sprite>(ecs::no_entity)
-			.with<SpriteAnimation>(Spritesheet::get_by_name("pong/pad"))
-			.with<Position>(geometry::Vec2{ 50, 300 })
+			.with<SpriteAnimation>(Spritesheet::get_by_name("test/WizardIdle"))
+			.with<Position>(geometry::Vec2{ 50, 600 - 32 })
 			.with<Visibility>(true)
-			.with<KeyBindings>(KeyCode::KEY_W, KeyCode::KEY_S)
+			.with<KeyBindings>(KeyCode::KEY_W, KeyCode::KEY_S, KeyCode::KEY_A, KeyCode::KEY_D)
 			.done();
 
-		auto pad_right = spawn()
+		auto player_two = spawn()
 			.with<Player>()
 			.with<Sprite>(ecs::no_entity)
-			.with<SpriteAnimation>(Spritesheet::get_by_name("pong/pad"))
-			.with<Position>(geometry::Vec2{ 750, 300 })
+			.with<SpriteAnimation>(Spritesheet::get_by_name("test/WizardIdle"))
+			.with<Position>(geometry::Vec2{ 750, 600 - 32 })
 			.with<Visibility>(true)
-			.with<KeyBindings>(KeyCode::KEY_UP, KeyCode::KEY_DOWN)
-			.done();
-
-		auto ball = spawn()
-			.with<Sprite>(ecs::no_entity)
-			.with<SpriteAnimation>(Spritesheet::get_by_name("pong/ball"))
-			.with<Position>(geometry::Vec2{ 300, 100 })
-			.with<Visibility>(true)
-			.with<Ball>(geometry::Vec2{ 1, 0 })
+			.with<KeyBindings>(KeyCode::KEY_UP, KeyCode::KEY_DOWN, KeyCode::KEY_LEFT, KeyCode::KEY_RIGHT)
 			.done();
 	}
 };
@@ -176,7 +104,7 @@ int main(int argc, char* argv[])
 
 	engine.setup<Default2D>();
 
-	Pong game;
+	SWMG game;
 	engine.run();
 
 	return 0;
