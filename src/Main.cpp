5d7190f3ba4e5342ae391cfc322a9bc33a52bf55
DaniloMatic99 <maticdaca.99@gmail.com>
2023-03-27 20:47:13 +0200

    Dodavanje igraca na scenu i spritesheet-a

--- src/Main.cpp
+++ src/Main.cpp
@@ -5,102 +5,22 @@
 
 using namespace core;
 
-struct PadCollisionSignal {
-	ecs::Entity ball;
-	ecs::Entity pad;
-};
-
 struct Player {};
-
-struct Ball 
-{
-	geometry::Vec2 speed;
-};
+struct Background {};
 
 struct KeyBindings 
 {
 	KeyCode up;
 	KeyCode down;
+	KeyCode right;
+	KeyCode left;
 };
 
-struct PhysicsSystem
-	: public ecs::System
-	, public AccessGroupStorage<Player, Position>
-	, public AccessGroupStorage<Ball, Position>
-	, public SignalEmitter<PadCollisionSignal>
-{
-	using QueryPlayers = AccessGroupStorage<Player, Position>;
-	using QueryBalls = AccessGroupStorage<Ball, Position>;
-
-	Bool intersects(const geometry::Vec2& pad_center, const geometry::Vec2& ball_center, float ball_radius)
-	{
-		static constexpr const int pad_width = 11;
-		static constexpr const int pad_height = 32;
-
-		const geometry::Vec2 circle_distance{
-			abs(ball_center.x - pad_center.x),
-			abs(ball_center.y - pad_center.y)
-		};
-
-		if (circle_distance.x > (pad_width + ball_radius)) { return false; }
-		if (circle_distance.y > (pad_height + ball_radius)) { return false; }
-
-		if (circle_distance.x <= pad_width) { return true; }
-		if (circle_distance.y <= pad_height) { return true; }
-
-		const auto dx = circle_distance.x - pad_width;
-		const auto dy = circle_distance.y - pad_height;
-		const auto corner_distance = dx * dx + dy * dy;
-
-		return corner_distance <= ball_radius * ball_radius;
-	}
-
-	void on_tick() override
-	{
-		for (auto&& [ball_entity, ball, pos] : QueryBalls::access_storage().each())
-		{
-			for (auto&& [player_entity, pad] : QueryPlayers::access_storage().each())
-			{
-				if (intersects(pad.xy, pos.xy, 16))
-				{
-					emit(PadCollisionSignal{ ball_entity, player_entity });
-				}
-			}
-		}
-	}
-};
 
 #define SCREEN_WIDTH 800
 #define SCREEN_HEIGHT 600
 #define BALL_RADIUS 16
 #define SPEED_MOD 200.0f
-
-struct BallMovementSystem
-	: public ecs::System
-	, public MutAccessGroupStorage<Ball, Position>
-	, public MutAccessComponentById<Ball>
-	, public SignalProcessor<PadCollisionSignal>
-{
-	virtual void process_signal(PadCollisionSignal& signal)
-	{
-		auto& ball = MutAccessComponentById<Ball>::get(signal.ball);
-		ball.speed.x *= -1;
-		ball.speed.y = get_random(-0.8f, 0.8f);
-	}
-
-	void on_tick() override
-	{
-		for (auto&& [entity, ball, pos] : access_storage().each())
-		{
-			if (pos.xy.y >= SCREEN_HEIGHT - BALL_RADIUS || pos.xy.y <= BALL_RADIUS)
-			{
-				ball.speed.y *= -1;
-			}
-
-			pos.xy += ball.speed * SPEED_MOD * Time::delta_time();
-		}
-	}
-};
 
 struct PlayerControlsSystem
 	: public ecs::System
@@ -125,46 +45,36 @@ struct PlayerControlsSystem
 			{
 				pos.xy.y += SPEED_MOD * Time::delta_time();
 			}
+			else if (keys.is_down(bindings.right))
+			{
+				pos.xy.x -= SPEED_MOD * Time::delta_time();
+			}
+			else if (keys.is_down(bindings.left))
+			{
+				pos.xy.x += SPEED_MOD * Time::delta_time();
+			}
+		
 		}
 	}
 };
 
-struct Pong : public Game
+struct FallingHero : public Game
 {
-	Pong()
+	FallingHero()
 	{
 		auto& engine = Engine::get_instance();
 		engine.use<PlayerControlsSystem>();
-		engine.use<BallMovementSystem>();
-		engine.use<PhysicsSystem>();
 	}
 
 	void on_start() override
 	{
-		auto pad_left = spawn()
+		auto hero = spawn()
 			.with<Player>()
 			.with<Sprite>(ecs::no_entity)
-			.with<SpriteAnimation>(Spritesheet::get_by_name("pong/pad"))
-			.with<Position>(geometry::Vec2{ 50, 300 })
-			.with<Visibility>(true)
-			.with<KeyBindings>(KeyCode::KEY_W, KeyCode::KEY_S)
-			.done();
-
-		auto pad_right = spawn()
-			.with<Player>()
-			.with<Sprite>(ecs::no_entity)
-			.with<SpriteAnimation>(Spritesheet::get_by_name("pong/pad"))
-			.with<Position>(geometry::Vec2{ 750, 300 })
-			.with<Visibility>(true)
-			.with<KeyBindings>(KeyCode::KEY_UP, KeyCode::KEY_DOWN)
-			.done();
-
-		auto ball = spawn()
-			.with<Sprite>(ecs::no_entity)
-			.with<SpriteAnimation>(Spritesheet::get_by_name("pong/ball"))
+			.with<SpriteAnimation>(Spritesheet::get_by_name("fallingHero/heroStanding"))
 			.with<Position>(geometry::Vec2{ 300, 100 })
 			.with<Visibility>(true)
-			.with<Ball>(geometry::Vec2{ 1, 0 })
+			.with<KeyBindings>(KeyCode::KEY_W, KeyCode::KEY_S, KeyCode::KEY_A, KeyCode::KEY_D)
 			.done();
 	}
 };
@@ -176,7 +86,8 @@ int main(int argc, char* argv[])
 
 	engine.setup<Default2D>();
 
-	Pong game;
+
+	FallingHero game;
 	engine.run();
 
 	return 0;
