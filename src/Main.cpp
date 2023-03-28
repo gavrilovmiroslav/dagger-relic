d4619e6b0ec9ad983a839866e8f7e118f19b9560
harry.cpp <harry.cpp@outlook.com>
2023-03-28 14:25:53 +0200

    setup meson for Windows

--- src/Main.cpp
+++ src/Main.cpp
@@ -169,11 +169,11 @@ struct Pong : public Game
 	}
 };
 
-#undef main
 int main(int argc, char* argv[])
 {
 	auto& engine = Engine::get_instance();
-	engine.configure("dagger.ini", argc, argv);
+	std::string filePath = "dagger.ini";
+	engine.configure(filePath.data(), argc, argv);
 
 	engine.setup<Default2D>();
 
