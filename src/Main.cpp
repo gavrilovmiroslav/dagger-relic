783aff364d12c919d5b7afc07bed3dde2b90d497
Jovan <jormundur00@gmail.com>
2023-03-29 13:47:52 +0200

    feature: Implemented primitive combat system
    Players can now shoot fireballs at eachother by pressing the newly added attack keys (F for player 1, ; for player 2).
    The current iteration of the game will despawn players after they have been hit by a fireball.
    Fireball sprite scaling was done manually, by adding a Scale component to the entity, but there must be a better way to do this, by extracting data from the sprite .data file.

--- src/Main.cpp
+++ src/Main.cpp
@@ -20,11 +20,18 @@ struct Pickup {
 	bool is_picked;
 };
 
+struct BasicAttack {
+	float speed;
+	float range;
+	float radius;
+};
+
 struct KeyBindings {
 	KeyCode up;
 	KeyCode down;
 	KeyCode left;
 	KeyCode right;
+	KeyCode basic_attack;
 };
 
 #define SCREEN_WIDTH 800
@@ -98,10 +105,31 @@ struct PlayerControlsSystem
 			if (keys.is_released(bindings.up)){
 				player.jump_speed/=2;
 			}
+			if (keys.is_pressed(bindings.basic_attack)){
+				if (flip == None){
+					auto attack = spawn()
+					.with<BasicAttack>(300.0f, pos.xy.x + 400.0f, 25.0f)
+					.with<Sprite>(ecs::no_entity)
+					.with<SpriteAnimation>(Spritesheet::get_by_name("test/Fireball"))
+					.with<Position>(geometry::Vec2{ pos.xy.x + 30.0f , pos.xy.y })
+					.with<Visibility>(true)
+					.with<Flip>(None)
+					.with<Scale>(geometry::Vec2{0.1f,0.1f}); // There must exist a better way to scale objects.
+				}
+				else{
+					auto attack = spawn()
+					.with<BasicAttack>(300.0f, pos.xy.x - 400.0f, 25.0f)
+					.with<Sprite>(ecs::no_entity)
+					.with<SpriteAnimation>(Spritesheet::get_by_name("test/Fireball"))
+					.with<Position>(geometry::Vec2{ pos.xy.x - 30.0f , pos.xy.y })
+					.with<Visibility>(true)
+					.with<Flip>(Horizontal)
+					.with<Scale>(geometry::Vec2{0.1f,0.1f}); // There must exist a better way to scale objects.
+				}
+			}
 		}
 	}
 };
-
 
 struct PickupSystem
 	: public ecs::System
@@ -159,6 +187,41 @@ struct PlatformSystem
 	}
 };
 
+struct CombatSystem
+	: public ecs::System
+	, public MutAccessGroupStorage<Player, Position>
+	, public MutAccessGroupStorage<BasicAttack, Position, SpriteAnimation, Flip>
+{
+	using QueryPlayers = MutAccessGroupStorage<Player, Position>;
+	using QueryAttacks = MutAccessGroupStorage<BasicAttack, Position, SpriteAnimation, Flip>;
+
+	void on_tick() override
+	{
+		for(auto&& [attack_entity, attack, attack_pos, sprite, flip] : QueryAttacks :: access_storage().each()){
+			if(flip == None){
+				attack_pos.xy.x += attack.speed * Time::delta_time();
+				if(attack_pos.xy.x > attack.range){
+					despawn(attack_entity);
+				}
+			}
+			else{
+				attack_pos.xy.x -= attack.speed * Time:: delta_time();
+				if(attack_pos.xy.x < attack.range){
+					despawn(attack_entity);
+				}
+			}
+
+			for(auto&& [player_entity, player, player_pos] : QueryPlayers :: access_storage().each()){
+				if (	abs(player_pos.xy.x - attack_pos.xy.x) <= attack.radius
+					&& 	abs(player_pos.xy.y - attack_pos.xy.y) <= attack.radius){
+						despawn(player_entity); // Just completely despawns the guy, for now.
+						despawn(attack_entity);
+					}
+			}
+		}
+	}
+};
+
 struct SWMG : public Game {
 	SWMG()
 	{
@@ -167,6 +230,7 @@ struct SWMG : public Game {
 		engine.use<GravitySystem>();
 		engine.use<PlatformSystem>();
 		engine.use<PickupSystem>();
+		engine.use<CombatSystem>();
 	}
 
 	void on_start() override{
@@ -233,7 +297,7 @@ struct SWMG : public Game {
 			.with<SpriteAnimation>(Spritesheet::get_by_name("test/WizardIdle"))
 			.with<Position>(geometry::Vec2{ 50, 600 - 32})
 			.with<Visibility>(true)
-			.with<KeyBindings>(KeyCode::KEY_W, KeyCode::KEY_S, KeyCode::KEY_A, KeyCode::KEY_D)
+			.with<KeyBindings>(KeyCode::KEY_W, KeyCode::KEY_S, KeyCode::KEY_A, KeyCode::KEY_D, KeyCode::KEY_F)
 			.with<Flip>(None)
 			.done();
 
@@ -243,8 +307,8 @@ struct SWMG : public Game {
 			.with<SpriteAnimation>(Spritesheet::get_by_name("test/WizardIdle"))
 			.with<Position>(geometry::Vec2{ 750, 600 - 32})
 			.with<Visibility>(true)
-			.with<KeyBindings>(KeyCode::KEY_UP, KeyCode::KEY_DOWN, KeyCode::KEY_LEFT, KeyCode::KEY_RIGHT)
-			.with<Flip>(None)
+			.with<KeyBindings>(KeyCode::KEY_UP, KeyCode::KEY_DOWN, KeyCode::KEY_LEFT, KeyCode::KEY_RIGHT, KeyCode::KEY_APOSTROPHE)
+			.with<Flip>(Horizontal)
 			.done();
 	}
 };
