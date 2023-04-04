fd8bf2e3fd13c2bba3d64ace2ab18982a5123332
Aleksandar Urosevic <alek.sandar0@yandex.com>
2023-04-04 22:27:03 +0200

    Implement systems for PushPlate, remove Trigger

--- include/PushPlate.h
+++ include/PushPlate.h
@@ -1,7 +1,54 @@
 #include "Definitions.h"
+#include "Access.h"
 
 struct PushPlate
 {
-    bool     active = false; /* If being pressed by something.      */
-    uint32_t tid    = 0;     /* Unique ID of thing to trigger or 0. */
+    Bool active         = false; /* If being pressed by something. */
+    Bool activeprevious = false; /* Previous state of 'active'.    */
+    tid  tid            = 0;     /* Unique ID of this PushPlate.   */
+};
+
+struct PushPlateSignal
+{
+    tid  tid    = 0;     /* ID of the PushPlate that was triggered. */
+    bool active = false; /* Current state of the PushPlate. */
+};
+
+struct PushPlateAllActiveSignal
+{
+    U32 count = 0; /* Number of PushPlates in the scene (that are also all active now). */
+};
+
+struct PushPlateCounter
+{
+    U32 count        = 0; /* Number of PushPlates in the scene. */
+    U32 count_target = 4; /* Number of PushPlates needed to be active for PushPlateAllActiveSignal to be emitted. */
+};
+
+/*
+ * Emit PushPlateSignal when PushPlate.active changes state.
+ */
+class PushPlateTriggerSystem 
+    : public ecs::System
+    , public MutAccessStorage<PushPlate>
+    , public SignalEmitter<PushPlateSignal>
+{
+public:
+    void on_tick() override;
+};
+
+/*
+ * Answer to PushPlateSignal, increment PushPlateCounter.count.
+ * When count_target is reached, emit PushPlateAllActiveSignal.
+ */
+class PushPlateCounterSystem
+    : public ecs::System
+    , public MutAccessUnique<PushPlateCounter>
+    , public SignalEmitter<PushPlateAllActiveSignal>
+    , public SignalProcessor<PushPlateSignal>
+{
+private:
+
+public:
+    void process_signal(PushPlateSignal&) override;	
 };
