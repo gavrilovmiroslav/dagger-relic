6b727b0d8eae1cd7693f7dc0e7d9537aa18031c8
Pavle Ciric <pavle@ciric.me>
2023-05-11 11:39:15 +0200

    quickfix: fixed animation freezing when collecting pickup
    Instead on destorying the entity of the collected pickup, we just delete
    it's visibility and pickup components.

--- include/Pickup.h
+++ include/Pickup.h
@@ -8,9 +8,12 @@ struct PickupSystem
 	: public ecs::System
 	, public MutAccessGroupStorage<Player, Position>
 	, public MutAccessGroupStorage<Pickup, Position>
+    , public MutAccessComponentById<Item>
+    , public MutAccessComponentById<Visibility>
 {
 	using QueryPlayers = MutAccessGroupStorage<Player, Position>;
 	using QueryPickups = MutAccessGroupStorage<Pickup, Position>;
+    using ItemGetter = MutAccessComponentById<Item>;
 	
 	void on_tick() override;
 };
--- src/Pickup.cpp
+++ src/Pickup.cpp
@@ -1,24 +1,24 @@
 #include "Pickup.h"
+#include "Access.h"
 #include <iostream>
 
 void PickupSystem::on_tick()
 {
     for (auto&& [player_entity, player, player_pos] : QueryPlayers::access_storage().each())
     {
-        std::vector<entt::entity> to_remove;
         for (auto&& [pickup_entity, pickup, pickup_pos] : QueryPickups::access_storage().each())
         {
             if (abs(player_pos.xy.x - pickup_pos.xy.x) <= pickup.radius &&
              	abs(player_pos.xy.y - pickup_pos.xy.y) <= pickup.radius)
             {
-                replace_component<Item>(player_entity, pickup.name, pickup.uses); // Removing this line, in addition to the despawn line will stop the crashing issue.                
-                to_remove.push_back(pickup_entity);                
-            }
-        }
 
-        for (auto e : to_remove)
-        {
-            despawn(e);
+                Item* current_item = ItemGetter::get(player_entity);
+                current_item->name = pickup.name;
+                current_item->uses = pickup.uses;
+                
+                remove_component<Visibility>(pickup_entity);
+                remove_component<Pickup>(pickup_entity); 
+            }
         }
     }
 }
