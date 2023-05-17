d342cad8f5922be3eaeba02597b3b8af89822d5c
Jovan <jormundur00@gmail.com>
2023-05-17 14:56:57 +0200

    refactor: Balanced game
    Reduced the amount of fastballs a player has on pickup from 6 to 4.
    Added one more health potion to the item pool, to increase the frequency of it spawning.

--- src/Pickup.cpp
+++ src/Pickup.cpp
@@ -35,7 +35,7 @@ Bool PickupSystem::on_start()
 	auto& generator = MutAccessUnique<Generator>::access_unique();
 	std::vector<I32> x_coordinates = {100, 150, 200, 250, 300, 350, 400, 450, 500, 550, 600, 650, 700};
 	std::vector<I32> y_coordinates = {100, 150, 200, 250, 300, 350, 400, 450, 500, 550, 600, 650, 700};
-	std::vector<String> spells = {"curseball", "curseball", "curseball", "fastball", "fastball", "fastball", "healthpotion", "firebomb"};
+	std::vector<String> spells = {"curseball", "curseball", "curseball", "fastball", "fastball", "fastball", "healthpotion", "healthpotion", "firebomb"};
 
 	MutAccessUnique<PickupTimer>::access_unique().start();
 	generator.initialize_deques(x_coordinates, y_coordinates, spells);
@@ -62,7 +62,7 @@ void PickupSystem::process_signal(PickupTimeoutSignal& signal)
 	else if (spell_name == "fastball")
 	{
 		spawn()
-			.with<Pickup>("fastball" , 24.0f, 6)
+			.with<Pickup>("fastball" , 24.0f, 4)
 			.with<Sprite>(ecs::no_entity)
 			.with<SpriteAnimation>(Spritesheet::get_by_name("test/FastballTome"))
 			.with<Scale>(geometry::Vec2{0.8f, 0.8f})
